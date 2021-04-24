8ffc6cdadca61483a3359f2d8b7e4b1920dc620b
kevinamexo <kevin.amex@gmail.com>
2021-04-24 12:28:17 +0000

    Implemented Add Task

--- src/components/MainContent/ProjectOverlay.js
+++ src/components/MainContent/ProjectOverlay.js
@@ -1,16 +1,20 @@
-import React, { useEffect } from 'react'
+import React, {useState, useEffect } from 'react'
 import './ProjectOverlay.css'
 import {useSelectedProjectValue} from '../../context'
 import {useTasks} from '../../firebase-hooks'
 import Task from '../Sidebar/Projects/Task'
 import {AiOutlinePlus} from 'react-icons/ai'
+import {useOpenSidebarValue} from '../../context'
+
+import AddTaskMain from '../Sidebar/Projects/AddTaskMain/AddTaskMain'
 const ProjectOverlay=()=> {
 
     const {selectedProject} = useSelectedProjectValue()
     const {tasks, setTasks}= useTasks(selectedProject.projectId)
-    
+    const {openSidebar} = useOpenSidebarValue()
+    const [showAddTask, setShowAddTask]= useState(false)
     return (
-        <div className="project-overlay-container">
+        <div className={openSidebar?"project-overlay-container":"project-overlay-container-full"}>
             <div className="p-o-header">{selectedProject.name}</div>
             <div className="pO_task-list">
                 <ul className="project-overlay__tasks">
@@ -22,12 +26,21 @@ const ProjectOverlay=()=> {
 
                 </ul>
             </div>
-            <div className="pO-add-task">
-                <span>
-                    <AiOutlinePlus className="pO-add-task-plus"/>
-                    <p className="pO-add-task-name">Add Task</p>
-                </span>
-            </div>
+           
+            {showAddTask? 
+                <AddTaskMain setShowAddTask={setShowAddTask}/>:
+                <div className="pO-add-task"
+                    role="button"
+                    tabIndex={0}
+                    onClick={()=>setShowAddTask(true)}
+                >
+                    <span>
+                        <AiOutlinePlus className="pO-add-task-plus"/>
+                        <p className="pO-add-task-name">Add Task</p>
+                    </span>
+                </div>
+            
+            }   
 
         </div>
     )
--- src/components/Sidebar/Projects/AddTaskMain/AddTaskMain.js
+++ src/components/Sidebar/Projects/AddTaskMain/AddTaskMain.js
@@ -1,10 +1,111 @@
-import React from 'react'
+import React, {useState} from 'react'
+import {IoCalendarClearOutline, IoIosCalendar} from 'react-icons/io'
+import {MdLabelOutline} from 'react-icons/md'
+import moment from 'moment'
+import './AddTaskMain.css'
+import {firebase} from '../../../../firebase'
+import { useSelectedProjectValue } from '../../../../context'
+import { VscInbox } from 'react-icons/vsc'
 
-function AddTaskMain() {
+
+
+function AddTaskMain({setShowAddTask}) {
+    const [task, setTask]= useState('')
+    const [taskDate, setTaskDate]= useState('')
+    const [project, setProject]= useState('')
+    const {selectedProject}= useSelectedProjectValue()
+    
+    const addtask=()=>{
+        const projectId= project||selectedProject
+        let filterDate = ''
+        if(projectId === 'TODAY'){
+            filterDate= moment().format('DD/MMY/YYYY')
+        } else{
+            filterDate=""
+        }
+    
+        return(
+            task&&
+            projectId&&
+            firebase
+                .firestore()
+                .collection('tasks')
+                .add({
+                    archived:false,
+                    date: filterDate||taskDate,
+                    task,
+                    projectId,
+                    userId: '2irjij20349cuu204'
+                })
+                .then(()=>{
+                    setTask('')
+                    setProject('')
+                    setShowAddTask(false)
+                })
+        )
+    
+    }
+
+    
+
     return (
-        <div>
-            
+        <>
+            <div className="add-task-main">
+                <input 
+                    className="add-task-main__taskName"
+                    name="taskName" 
+                    value={task}
+                    type="text" 
+                    onChange={(e)=> setTask(e.target.value)}
+                    aria-label="Enter your task"
+                    placeholder="Enter your task name here"
+                    data-testid="add-task-content"
+                />
+                <div className="atm-timeFilter-icons">
+                    <span className="atm-calendar">
+                        <IoIosCalendar/>
+                        <p>Today</p>
+                    </span>
+                    <span className="atm-inbox">
+                        <VscInbox/>
+                        <p>Inbox</p>
+                    </span>
+                    <div className="atm-label">
+                        <MdLabelOutline className="atm-label-icon" />
+                    </div>
+            </div>
         </div>
+        <div className="atm-submit-cancel">
+            <button 
+                className="atm-submit-task"
+                tabIndex={0}
+                onKeyDown={(e)=>{
+                    if (e.key==="Enter"){
+                        addtask()
+                    }
+                }}
+                onClick={()=>addtask()}
+            >
+                Add Task
+            </button>
+            <p 
+                className="atm-cancel-submit-task"
+                tabIndex={0}
+                onKeyDown={(e)=>{
+                    if (e.key==="Enter"){
+                        setTask('')
+                        setTaskDate('')
+                        setShowAddTask(false)
+                    }
+                }}
+                onClick={()=>setShowAddTask(false)}
+            >
+                Cancel
+            </p>
+
+        </div>
+        
+    </>
     )
 }
 
