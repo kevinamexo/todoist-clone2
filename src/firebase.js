f8d3ffc56cb2f9c711ca3434321862f46ff827b8
Kevin <72938193+kevinamexo@users.noreply.github.com>
2021-06-14 15:15:53 +0000

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -4,12 +4,12 @@ import 'firebase/firestore'
 import 'firebase/auth'
 
 const firebaseConfig= firebase.initializeApp({
-    apiKey: "AIzaSyDFlcb9YA3aL7H9A2UfqfXUnLKtPsT0JUk",
-    authDomain: "react-auth-5953c.firebaseapp.com",
-    projectId: "react-auth-5953c",
-    storageBucket: "react-auth-5953c.appspot.com",
-    messagingSenderId: "838237792091",
-    appId: "1:838237792091:web:9c563e05b14da4cf645c7e"
+    apiKey: 
+    authDomain: 
+    projectId: 
+    storageBucket:
+    messagingSenderId:
+    appId:
 })
 export const auth= firebaseConfig.auth()
 export default firebaseConfig
